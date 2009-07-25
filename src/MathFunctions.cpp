89c7b295897510cb2a98690a0b9216abd585e065
John Erik Sloper <jesloper@gmail.com>
2009-07-25 11:57:06 +0200

    Added missing file

---
+++ src/MathFunctions.cpp
@@ -0,0 +1,59 @@
+#include "MathFunctions.h"
+#include <stdlib.h>
+#include "mersenne.cpp"
+#include <QtDebug>
+#include <ctime>
+TRandomMersenne rb((unsigned int)time(0)); 
+
+/**
+  * \brief creates a random number
+  *
+  * \param min minimum number
+  * \param max maximum number
+  * \return integer between (and including) min and max
+*/
+
+int getRandInt(int min, int max){
+	//qDebug() << "Rand between " << min  << " and " << max << "  = " << rb.IRandom(min,max);
+ return	rb.IRandom(min,max);
+ //double r = (double)rand()/(double(RAND_MAX)+1);
+ //return (int)(r*(max-min+1)+min);
+}
+
+int max(int f, int s){
+	if(f > s)
+		return f;
+	return s;
+}
+
+
+int findUnique(double *array, int size){
+	int unique = size;
+	std::map<double,double> unq;
+	std::map<double,double>::iterator it;
+	for(int i = 0; i < size;i++){
+		it = unq.find(array[i]);
+		if(it != unq.end()){
+			unique--;
+		}else{
+			unq[array[i]] = 1;
+		}
+	}
+	return unique;
+}
+double findMax(double* array, int size) {
+	double max = 0;
+	for (int i = 0; i <= size; i++) {
+		if (array[i] > max)
+			max = array[i];
+	}
+	return max;
+}
+double findMin(double* array, int size) {
+	double min = 0;
+	for (int i = 0; i <= size; i++) {
+		if (array[i] < min)
+			min = array[i];
+	}
+	return min;
+}
